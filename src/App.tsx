b9797b1ddd4743f7c2d61bffcfe667025514e3eb
Kalinina Oksana <ksan4ik92@gmail.com>
2024-03-20 16:29:42 +0300

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+
+import { useEffect } from 'react';
+import './App.css'
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const tg = (window as any).Telegram.WebApp;
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.close;
+  }
+
+  return (
+    <>
+      <div className="App">
+        <button onClick={onClose}>Закрыть</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
